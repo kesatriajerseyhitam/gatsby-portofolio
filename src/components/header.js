07bd74323a619932452afee96a373119fdb35032
Raka <mgf.prauliyatama@gmail.com>
2020-04-01 19:59:06 +0700

    feat(app): adding initial file

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,25 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
+
+import { Link } from "gatsby"
+
+import color from '../constant/color';
+
+const {
+  primaryBackground,
+} = color;
+
+const HeaderContainer = styled.div`
+  background: ${primaryBackground};
+  color: white;
+  height: 20vh;
+`
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <HeaderContainer>
+    Hallo mate
+  </HeaderContainer>
 )
 
 Header.propTypes = {
---
+++ src/constant/color.js
@@ -0,0 +1,13 @@
+const color = {
+  gradient: 'linear-gradient(90deg, rgba(242,148,40,1) 0%, rgba(178,37,82,1) 100%)',
+  graph: '#e632ac',
+  node: '#84ce24',
+  primaryBackground: '#202020',
+  primaryHighlight: '#f29428',
+  react: '#0090ff',
+  saga: '#89d96d',
+  secondaryBackground: '#252525',
+  secondaryHighlight: '#b22552',
+}
+
+export default color;
