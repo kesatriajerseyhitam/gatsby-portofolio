516ec2579e58ddc6443788125fe29acc1a95c67f
Raka <mgf.prauliyatama@gmail.com>
2020-04-15 23:04:54 +0700

    Change next config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,13 @@
 module.exports = {
   siteMetadata: {
     title: 'Tama\'s Project',
-    description: 'Auliya Raka Pratama online portofolio. Auliya Raka Pratama Front End Back End Portofolio. @prauliyatama online portofolio. @kesatriajerseyhitam',
+    description: `
+      My name is Raka Pratama, you can call me Tama. I am professional Software Engineer.
+      Have 2+ years experiences on developing website both Front-End and Back-End.
+      Most technologyI used is using Javascript, with a wide-range of frameworks. But,
+      the one that I frequently used and my personal choice is React for Front-End and
+      Express for Back-End. Beside of Javascript, I also use Dart and Go programming language.
+    `,
     author: '@prauliyatama',
   },
   plugins: [
